73d3d16ec82071a4ad8ff1e75e88ea78d062cdff
codawise-onyekachi <onyekachihopeuzoma@gmail.com>
2024-08-21 15:45:18 +0100

    Intro to Python Assignment

---
+++ Intro_python_user_input.py
@@ -0,0 +1,4 @@
+name = input("What is your name ? ")
+age = input("Please enter your age: ")
+location = input("Enter your location: ")
+print(f" Hello {name}, you are {age} years old and you live in {location}")
